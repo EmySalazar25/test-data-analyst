95572c2220fdf23f7786a46eb92b0fa097b43ee0
Emily Salazar <emysalazar25@gmail.com>
2022-07-25 09:05:18 -0500

    Add files via upload

---
+++ common.py
@@ -0,0 +1,27 @@
+#Librerias a utilizar:
+
+# Información de Paises
+import country_converter as coco
+
+# Graficas
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+
+# Obtención de los datos 
+import requests
+import json
+import re
+
+# Reporte de variables 
+from pandas_profiling import ProfileReport
+
+import pandas as pd 
+import numpy as np
+
+
+# Acomodo de fechas 
+from sorted_months_weekdays import *
+from sort_dataframeby_monthorweek import *
